d69e2d3bd8bbaf16b31920dad35cc52df9ac3ea3
jcpoulin <jcpoulin@ualberta.ca>
2020-02-24 12:04:32 -0700

    Removed all files that do not relate to yeg news

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'RxKIhy0eMijpqujc6vvTl95xO',
+    consumer_secret: 'ZjJ6JJWKTJ00JbDcWdgH5sjz5CGdm8Oud0SdPuX5GunYcPxsdM',
+    access_token: '1228091126621462528-Cf4H6MRNTa8gliYO2HKdtsryqOPDbW',
+    access_token_secret: '61sJK76IcKiDhgSQSG1tls0taBL5mzfqtRkAHKCtW8aGT',
+};
