9c9af6bee75fe765230dc1cc6f8f433809860779
jcpoulin <jcpoulin@ualberta.ca>
2020-02-29 21:28:50 -0700

    Changed port --> PORT

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const config = require('./config');
 const twit = require('twit');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const aws = require('aws-sdk');
 
 var app = require('express')();
